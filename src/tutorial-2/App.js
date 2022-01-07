106dd28182f2456ed41b5fc4c1356f71cf0580cc
Arbi Merzhoev <arbimerzhoev@mail.ru>
2022-01-07 22:54:49 +0300

    Убрал лишние консоль логи(незначительные правки)

--- src/tutorial-2/App.js
+++ src/tutorial-2/App.js
@@ -4,13 +4,11 @@ function App() {
   let email = '';
   let password = '';
 
-  function universalHandler(e) {
+  function handleInput(e) {
     if (e.target.type === 'text') {
       email = e.target.value;
-      console.log('email: ', email);
     } else {
       password = e.target.value;
-      console.log('password: ', password);
     }
   }
 
@@ -30,10 +28,10 @@ function App() {
     <div className="auth">
       <form onSubmit={handleSubmit}>
         <div className="auth__email">
-          <input onChange={universalHandler} type="text" />
+          <input onChange={handleInput} type="text" />
         </div>
         <div className="auth__password">
-          <input onChange={universalHandler} type="password" />
+          <input onChange={handleInput} type="password" />
         </div>
         <button type="submit">Войти</button>
       </form>
